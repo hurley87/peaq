87980e81a386bcfefd1b9436a6f34e28c4d4a086
David Hurley <dhurls99@gmail.com>
2024-08-25 10:32:01 -0400

    comment out deploy scripts

--- hardhat/ignition/modules/SafeMint.ts
+++ hardhat/ignition/modules/SafeMint.ts
@@ -1,11 +1,11 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+// import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const SafeMintModule = buildModule("SafeMintModule", (m) => {
-  const PUBLIC_KEY = '0x1169E27981BceEd47E590bB9E327b26529962bAe';
+// const SafeMintModule = buildModule("SafeMintModule", (m) => {
+//   const PUBLIC_KEY = '0x1169E27981BceEd47E590bB9E327b26529962bAe';
 
-  const safeMint = m.contract("SafeMint", [PUBLIC_KEY]);
+//   const safeMint = m.contract("SafeMint", [PUBLIC_KEY]);
 
-  return { safeMint };
-});
+//   return { safeMint };
+// });
 
-export default SafeMintModule;
+// export default SafeMintModule;
