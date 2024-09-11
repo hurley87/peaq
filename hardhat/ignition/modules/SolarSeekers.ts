1f6f08ea7058892b602e54396e21dacdfe5868bc
David Hurley <dhurls99@gmail.com>
2024-09-11 09:19:36 -0400

    comment out deploy scripts

--- hardhat/ignition/modules/SolarSeekers.ts
+++ hardhat/ignition/modules/SolarSeekers.ts
@@ -1,10 +1,10 @@
-import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
+// import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 
-const SolarSeekersModule = buildModule('SolarSeekersModule', (m) => {
-  const TRAITS_CONTRACT = '0x6C1D44C358dF67f401ff570b684A0B4167B58063';
-  const solarSeeker = m.contract('SolarSeekers', [TRAITS_CONTRACT]);
+// const SolarSeekersModule = buildModule('SolarSeekersModule', (m) => {
+//   const TRAITS_CONTRACT = '0x6C1D44C358dF67f401ff570b684A0B4167B58063';
+//   const solarSeeker = m.contract('SolarSeekers', [TRAITS_CONTRACT]);
 
-  return { solarSeeker };
-});
+//   return { solarSeeker };
+// });
 
-export default SolarSeekersModule;
+// export default SolarSeekersModule;
